2a3f40f2d5076c6e02ba8f57c43cf444fb6954e1
Andriy Dzikh <andriydzikh@gmail.com>
2017-05-06 21:08:12 -0400

    Improved the vertex colour shader. Added a menu... Kind of...

--- Assets/Materials/VertexColours.shader
+++ Assets/Materials/VertexColours.shader
@@ -1,13 +1,11 @@
 ﻿Shader "Custom/VertexColours" {
 	Properties {
 		_Color ("Color", Color) = (1,1,1,1)
-		_MainTex ("Albedo (RGB)", 2D) = "white" {}
-		_Glossiness ("Smoothness", Range(0,1)) = 0.5
-		_Metallic ("Metallic", Range(0,1)) = 0.0
 	}
 	SubShader {
 		Tags { "RenderType"="Opaque" }
 		LOD 200
+		Cull Off
 		
 		CGPROGRAM
 		// Physically based Standard lighting model, and enable shadows on all light types
@@ -16,15 +14,10 @@
 		// Use shader model 3.0 target, to get nicer looking lighting
 		#pragma target 3.0
 
-		sampler2D _MainTex;
-
 		struct Input {
-			float2 uv_MainTex;
 			float4 vertColour : COLOR;
 		};
 
-		half _Glossiness;
-		half _Metallic;
 		fixed4 _Color;
 
 		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
@@ -36,12 +29,11 @@
 
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			// Albedo comes from a texture tinted by color
-			fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
 			o.Albedo = IN.vertColour;
 			// Metallic and smoothness come from slider variables
-			o.Metallic = _Metallic;
-			o.Smoothness = _Glossiness;
-			o.Alpha = c.a;
+			o.Metallic = 0;
+			o.Smoothness = 0;
+			o.Alpha = 1;
 		}
 		ENDCG
 	}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject two;
+    public GameObject three;
+    public GameObject four;
+
+    public GameObject ticker;
+    public GameObject tickerPlayerTgt;
+    public GameObject tickerHTPTgt;
+    public GameObject tickerQuitTgt;
+
+    public float inputTime = .25f;
+
+    private int selectOptions = 0;
+
+    private int playerCount = 0;
+
+    private float timeSinceInput = 0;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        two.SetActive(playerCount == 0);
+        three.SetActive(playerCount == 1);
+        four.SetActive(playerCount == 2);
+
+        ticker.transform.position = selectOptions == 0 ? tickerPlayerTgt.transform.position : selectOptions == 1 ? tickerHTPTgt.transform.position : tickerQuitTgt.transform.position;
+
+        if(Time.time - timeSinceInput > inputTime)
+        {
+            if(Input.GetAxis("menu_select") > 0)
+            {
+                timeSinceInput = Time.time;
+                if(selectOptions == 0)
+                {
+                    Play();
+                }
+                else if (selectOptions == 1)
+                {
+                    HowToPlay();
+                }
+                else
+                {
+                    Quit();
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxis("menu_move_vert")) > 0)
+            {
+                timeSinceInput = Time.time;
+                selectOptions -= (int)Mathf.Sign(Input.GetAxis("menu_move_vert"));
+                if(selectOptions > 2)
+                {
+                    selectOptions = 0;
+                }
+                else if (selectOptions < 0)
+                {
+                    selectOptions = 2;
+                }
+            }
+            else if(Mathf.Abs(Input.GetAxis("menu_move_horiz")) > 0)
+            {
+                timeSinceInput = Time.time;
+                if (selectOptions == 0)
+                {
+                    playerCount += (int)Mathf.Sign(Input.GetAxis("menu_move_horiz"));
+                    if (playerCount > 2)
+                    {
+                        playerCount = 0;
+                    }
+                    else if (playerCount < 0)
+                    {
+                        playerCount = 2;
+                    }
+                }
+            }
+        }
+    }
+
+    void Play()
+    {
+        playerCount += 2;
+    }
+
+    void HowToPlay()
+    {
+
+    }
+
+    void Quit()
+    {
+
+    }
+}
